737742410f337d26b9f1ecc88264aec4a7749423
공채원 <chaewonkong@gmail.com>
2018-12-04 15:22:46 +0900

    Connect types to index action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+import { EMAIL_CHANGED, PASSWORD_CHANGED } from "./types";
+
 export const emailChanged = text => {
   return {
     // Action is a plain javascript object that returns object
     // that has a type property of string
-    type: "email_changed",
+    type: EMAIL_CHANGED,
     payload: text
   };
 };
