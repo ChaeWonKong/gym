0c7067b3ec1586cb71888fb21d6a30df7eb67d1f
공채원 <chaewonkong@gmail.com>
2018-12-04 14:17:08 +0900

    Add redux

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header headerText="Gym" />
-      </div>
+      <Provider store={createStore}>
+        <div className="App">
+          <Header headerText="Gym" />
+        </div>
+      </Provider>
     );
   }
 }
