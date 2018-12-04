488b9194adad575a6af81325d7ff79ad5873c8a2
공채원 <chaewonkong@gmail.com>
2018-12-04 14:10:28 +0900

    Initialize repo

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = props => {
+  return <div style={styles.headerStyle}>{props.headerText}</div>;
+};
+
+const styles = {
+  headerStyle: {
+    display: "flex",
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    background: "#bbb"
+  }
+};
+export default Header;
