8f50ed2526d6cb72693f76453d668dbd904d8029
공채원 <chaewonkong@gmail.com>
2018-12-04 14:27:35 +0900

    Wire up firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Header from "./components/Header";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-
+import firebase from "firebase";
+import logo from "./logo.svg";
+import "./App.css";
+import firebaseAuth from "./firebaseAuth.json";
+import Header from "./components/Header";
+import reducers from "./reducers";
 class App extends Component {
+  componentWillMount() {
+    const config = firebaseAuth;
+    !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
+  }
   render() {
     return (
-      <Provider store={createStore}>
+      <Provider store={createStore(reducers)}>
         <div className="App">
           <Header headerText="Gym" />
         </div>
