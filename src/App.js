a74d246b5f205ef28ad64450f2a89d29b8050047
공채원 <chaewonkong@gmail.com>
2018-12-05 16:36:43 +0900

    Add conditional rendering function / firebase.auth

--- src/App.js
+++ src/App.js
@@ -10,16 +10,38 @@ import { Header } from "./components/common";
 import LoginForm from "./components/LoginForm";
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     const config = firebaseAuth;
     !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <div>success</div>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <div>Wait...</div>;
+    }
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <div className="App">
           <Header headerText="Gym" />
-          <LoginForm />
+          {this.renderContent()}
         </div>
       </Provider>
     );
