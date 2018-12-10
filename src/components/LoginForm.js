6f9dbdce9ca20ac3392ea0acf1dbd847e7382179
공채원 <chaewonkong@gmail.com>
2018-12-10 10:27:55 +0900

    Add password in redux

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { emailChanged } from "../actions";
+import { emailChanged, passwordChanged } from "../actions";
 
 class LoginForm extends Component {
   handleEmailChange(e) {
     this.props.emailChanged(e.target.value);
   }
 
+  handlePasswordChange(e) {
+    this.props.passwordChanged(e.target.value);
+  }
+
   render() {
-    console.log(this.props.email);
+    console.log(this.props.password);
     const { container, idContainer, pwContainer } = styles;
     return (
       <div style={container}>
@@ -24,7 +28,13 @@ class LoginForm extends Component {
 
         <div style={pwContainer}>
           <label style={{ marginRight: 10 }}>Password</label>
-          <input label="Password" placeholder="password" />
+          <input
+            label="Password"
+            type="password"
+            placeholder="password"
+            onChange={this.handlePasswordChange.bind(this)}
+            value={this.props.password}
+          />
         </div>
         <a className="waves-effect waves-light btn">Login</a>
       </div>
@@ -58,11 +68,12 @@ const styles = {
 
 const mapStateToProps = state => {
   return {
-    email: state.auth.email
+    email: state.auth.email,
+    password: state.auth.password
   };
 };
 
 export default connect(
   mapStateToProps,
-  { emailChanged }
+  { emailChanged, passwordChanged }
 )(LoginForm);
