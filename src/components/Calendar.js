d6b5c6de05647454d458c3a6f55c16a10582bf2a
공채원 <chaewonkong@gmail.com>
2018-12-21 18:19:49 +0900

    Convert Calendar to component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,53 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Calendar = () => {
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>Sat</th>
-            <th>Sun</th>
-          </tr>
-        </thead>
+class Calendar extends Component {
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Mon</th>
+              <th>Tue</th>
+              <th>Wed</th>
+              <th>Thu</th>
+              <th>Fri</th>
+              <th>Sat</th>
+              <th>Sun</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-          </tr>
-          <tr>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-          </tr>
-          <tr>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-            <td>Alvin</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
+          <tbody>
+            <tr>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+            </tr>
+            <tr>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+            </tr>
+            <tr>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+              <td>Alvin</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default Calendar;
